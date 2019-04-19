b11555747c926aa548156d0eb9441805c1414121
izaak-coleman <thenameisizaak@gmail.com>
2019-04-19 18:28:55 -0400

    Uploading the days work (commit description fail)

--- enrichment_analysis/kegg_analysis/OriTerKeggScrape.py
+++ enrichment_analysis/kegg_analysis/OriTerKeggScrape.py
@@ -4,6 +4,8 @@ import urllib.request
 import collections
 import re
 from subprocess import Popen, PIPE
+import gzip
+
 
 import KeggNames
 kn = KeggNames.keggnames
@@ -16,18 +18,11 @@ Ori = collections.namedtuple('Ori', ['seq','len','start','end'])
 
 def get_kegg_organism_list():
   url = 'http://rest.kegg.jp/list/organism'
-  return urllib.request.urlopen(url).read().decode('utf-8').split('\n')
+  data = urllib.request.urlopen(url).read().decode('utf-8').split('\n')
+  data.pop()
+  return data
 
-def search_ncbi_for_refseq_id(gb_id):
-  cmd = f"esearch -db nuccore -query {gb_id} | elink -target nuccore -name nuccore_nuccore_gbrs | efetch -format docsum | xtract -pattern DocumentSummary -element AccessionVersion Slen Title | sed 's/,.*//' | sort -t $'\t' -k 2,2nr"
-  result,err = Popen(cmd, stdout=PIPE,stderr=PIPE,shell=True).communicate()
-  result = result.decode('utf-8').strip().split('\n')
-  if len(result) != 1:
-    log = open('multiple_refseq_ids.log','a+')
-    log.write('\n'.join(result))
-    log.close()
-    return result[0].split('\t')[0]
-  return result[0].split('\t')[0]
+
 
 class Organism:
   """Container class storing data scraped from KEGG, NCBI and TUBIC relating
@@ -38,8 +33,8 @@ class Organism:
     self.kegg_organism_id = kegg_organism_id # e.g hsa
     self.strain_name      = strain_name      # e.g Homo sapiens (human)
     self.taxa_string      = taxa_string      # e.g Eukaryotes;Animals;Vertebrates;Mammals
-    self.genes = dict()                      # k='gene ids', v='kegg info, gene location info, etc'
-    self.gff 
+    self.annotated_gff = []
+    self.get_metadata()
 
 
   def in_taxonomic_group(self, group):
@@ -51,6 +46,17 @@ class Organism:
     # Download webpage from kegg, read, and convert to regular str type
     return urllib.request.urlopen(url).read().decode('utf-8')
 
+  def search_ncbi_for_refseq_id(self, gb_id):
+    cmd = f"esearch -db nuccore -query {gb_id} | elink -target nuccore -name nuccore_nuccore_gbrs | efetch -format docsum | xtract -pattern DocumentSummary -element AccessionVersion Slen Title | sed 's/,.*//' | sort -t $'\t' -k 2,2nr"
+    result,err = Popen(cmd, stdout=PIPE,stderr=PIPE,shell=True).communicate()
+    result = result.decode('utf-8').strip().split('\n')
+    if len(result) != 1:
+      log = open(self.kegg_organism_id + '_multiple_refseq_ids.log','w')
+      log.write('\n'.join(result))
+      log.close()
+      return result[0].split('\t')[0]
+    return result[0].split('\t')[0]
+
 
   def get_metadata(self):
     """Makes request to rest.kegg.jp/get/kegg_genome_id to get various metadata:
@@ -60,7 +66,7 @@ class Organism:
     entry = self.get_genome_entry_from_kegg()
     # Make regex engine
     match = lambda a, b : re.findall(rf'{a}\s+(\S.*)\n',b)[0]
-    log = open('failed_metadata_retrieval.log','a+')
+    log = open(self.kegg_genome_id + '_failed_metadata_retrieval.log','w')
     # Resolve basic metadata
     try:
       self.genome_length = int(match(kn.LENGTH, entry))
@@ -71,6 +77,8 @@ class Organism:
       if self.taxa_string == None:
         self.taxa_string = match(kn.LINEAGE, entry)
 
+      self.assembly = match(kn.DATA_SOURCE, entry)
+      self.assembly = re.findall('Assembly:([\.\w]+)', self.assembly)[0]
       # Attempt to determine the Refseq sequence id used for this kegg organism
       seq = match(kn.SEQUENCE, entry)
       if seq[0:2] == 'RS':
@@ -80,37 +88,90 @@ class Organism:
         self.rs_id = search_ncbi_for_refseq_id(self.gb_id)
     except:
       log.write(entry)
-    
-  def get_kegg_data_for_genes(self):
-    """Makes request to rest.kegg.jp/list/kegg_organim_id to get a list of geneids
-       then for each gene_id makes request to rest.kegg.jp/get/gene_id to get
-       the kegg info each gene. This is then parsed and the (k=gene_id, v=kegg_data)
-       pair is stored in the self.gene dictionary."""
-    url = f'http://rest.kegg.jp/link/pathway/{self.kegg_genome_id}'
-    gene_pw_list = urllib.request.urlopen(url).read().decode('utf-8').split('\n')
-    gene_pw_list = [tuple(e.split('\t') for e in gene_pw_list)]
-    self.genes = {gid, self.get_gene_data(gid,pw) for gid, pw in gene_pw_list}
+    log.close()
 
-  def get_gene_data(self, gid, pw):
-    url = f'http://rest.kegg.jp/link/pathway/{self.kegg_genome_id}'
+  def generate_index(self, gff, field):
+    match = lambda a,b : re.findall(rf'[^\w]{a}=([\w]+)',b)
+    d = dict()
+    for index, line in enumerate(gff):
+      tag = match(field, line)
+      if len(tag) == 1:
+        d[tag[0]] = index
+    return d
+
+  def set_gff_filename(self, file_name):
+    self.gff_filename = file_name
+
+  def kegg_annotate_gff(self):
+    """Adds KEGG pathway, Ontology and other annotations to gff"""
+    if len(self.annotated_gff) == 0:
+      with gzip.open(self.gff_filename, 'rb') as f:
+        gff = f.read().decode('utf-8').split('\n')
+        gff.pop()
+        gff = [g for g in gff if g[0] != '#']
+        gff = [g for g in gff if (g.split('\t')[2] == 'gene' and g.split('\t')[0] == 'NC_002663.1')]
+        self.annotated_gff = gff
+
+    old_tag_idx = self.generate_index(gff, 'old_locus_tag')
+    tag_idx = self.generate_index(gff, 'locus_tag')
+    annotations = self.get_kegg_gene_annotations('pathway')
+    remove_header = lambda x : re.findall(r'.*:(.*)',x)[0]
+
+    log = open(self.kegg_genome_id + '_failed_kegg_annotation.log','w')
+    for gene_id, kegg_anno in annotations:
+      gene_id = remove_header(gene_id)
+      idx = old_tag_idx.get(gene_id, -1) 
+      if idx == -1:
+        idx = tag_idx.get(gene_id, -1)
+      if idx == -1:
+        log.write(gene_id + '\n')
+        continue
+      self.annotated_gff[idx] += '\t' + kegg_anno
+    log.close()
+
+  def write_annotated_gff(self):
+    data = [(int(e.split('\t')[3]),e) for e in self.annotated_gff]
+    data = sorted(data, key=lambda x: x[0])
+    self.annotated_gff = [e for _,e in data]
+    with gzip.open(self.gff_filename, 'wb') as f:
+      f.write('\n'.join(self.annotated_gff))
+
+
+    
+  def get_kegg_gene_annotations(self, annotation):
+    url = f'http://rest.kegg.jp/link/{annotation}/{self.kegg_genome_id}'
+    data = urllib.request.urlopen(url).read().decode('utf-8').split('\n')
+    data = [tuple(e.split('\t')) for e in data]
+    data.pop()
+    return data
 
   def set_ori_info(self, tubic_entry):
-    """Parses the tubic_entry and extracts relevant info to make self.Ori object."""
+    """Parses the tubic_entry and extracts relevant info to make self. Ori object."""
     pass
 
 
 def main():
   olist = get_kegg_organism_list()
+  olist = [org for org in olist if 'Bacteria' in org.split('\t')[3]]
   # Generator produces an Organism for each organism in kegg in relevant taxanomic rank
-  orgs = (Organism(*org.split('\t')) 
-            for org in olist
-            if ('Bacteria' in Organism(*org.split('\t')).taxa_string))
+  #orgs = (Organism(*org.split('\t')) 
+  #            for org in olist
+  #            if ('Bacteria' in Organism(*org.split('\t')).taxa_string))
+
+  pmu = [e for e in olist if 'pmu' in e.split('\t')[1]][0]
+  pmu = Organism(*pmu.split('\t'))
+  pmu.set_gff_filename('../doric10/gffs/gff/GCF_000006825.1_ASM682v1_genomic.gff.gz')
+  pmu.kegg_annotate_gff()
+  print('\n'.join(pmu.annotated_gff))
 
 
-  for org in orgs:
-    org.get_metadata()
 
 
 if __name__ == '__main__':
   main()
 
+
+# to do
+# 1. Fix kegg_annotate_gff such that files are filtered for ncbi refseq
+# 2. Figure out the actual main.py pipeline. this will help to determine when Organism knows what
+# 3. Switch to use of assemblies instead of relying on ncbi refseq this seems to fail in many cases
