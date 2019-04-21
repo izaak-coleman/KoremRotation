fadf7526498f2b568ec56e7926e1e0e58a11a1a2
izaak-coleman <thenameisizaak@gmail.com>
2019-04-21 17:24:32 -0400

    Finished modifying assembly

--- enrichment_analysis/kegg_analysis/Assembly.py
+++ enrichment_analysis/kegg_analysis/Assembly.py
@@ -1,6 +1,8 @@
 import collections
+import sys
+import re
 
-Assembly = collection.namedtuple('Assembly', ['filename', 'assembly', 'refseq', 'length','n_contigs'])
+Assembly = collections.namedtuple('Assembly', ['filename', 'assembly', 'refseq', 'length','contigs'])
 
 def get_assembly_info(assembly_data):
   with open(assembly_data) as f:
@@ -9,19 +11,25 @@ def get_assembly_info(assembly_data):
   entries = [data[ delimiters[i] : delimiters[i+1] ] for i in range(0, len(delimiters)-1)]
   entries.append(data[delimiters[-1]:])
   get_assembly = lambda x: re.findall(r'genome-build-accession.*:(GCF[\w\.]+)#',x)
-  get_refseqs  = lambda x: re.findall(r'sequence-region ([\w\.]+) [0-9]+ ([0-9]+)#',x)
+  get_contigs = lambda x: re.findall(r'sequence-region ([\w\.]+) [0-9]+ ([0-9]+)#',x)
 
   assemblies = []
   for entry in entries:
     filename = entry.pop(0)
     entry = ''.join(entry) + '#'
     assembly = get_assembly(entry)
-    refseqs = get_refseqs(entry)
-    refseqs = sorted([(refseq, int(length)) for refseq, length in refseqs], lambda x : x[1])
-    refseq, length = refseq[0]
-    assemblies.append(Assembly(filename, assembly, refseq, length, len(refseqs)))
+    contigs = get_contigs(entry)
+    contigs = sorted([(contig, int(length)) for contig, length in contigs], key=lambda x : x[1])
+    assemblies.append(Assembly(filename, assembly, '', -1, contigs))
   return assemblies
 
 def main():
-  pass
-#test this!
+  if len(sys.argv) != 2:
+    print('Usage <exe> <assembly.dat>')
+    sys.exit()
+  assemblies = get_assembly_info(sys.argv[1])
+  for a in assemblies[:10]:
+    print(f'{a.filename} {a.assembly} {a.refseq} {a.length} {a.contigs}')
+
+if __name__ == '__main__':
+  main()
