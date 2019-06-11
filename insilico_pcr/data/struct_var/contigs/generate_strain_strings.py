741a4d2df803cb4ab540b08911848bb5528fa58f
Izaak Coleman <ic2465@c2b2rrld5.arcs.cpmc.columbia.edu>
2019-06-11 17:12:57 -0400

    Pushing contig construction code, needs to be fixed (single string, no line breaks

---
+++ insilico_pcr/data/struct_var/contigs/generate_strain_strings.py
@@ -0,0 +1,26 @@
+import sys
+import csv
+import gzip
+
+def main():
+  if len(sys.argv) != 3:
+    print("Usage: <exe> <contig_concat> <path_to_contigs>")
+    sys.exit()
+
+  with open(sys.argv[1]) as f:
+    contig_list = [line for line in csv.reader(f)]
+    contig_list.pop(0)
+
+  for strain, contig_id, pos in contig_list:
+    print(contig_id)
+    with gzip.open(sys.argv[2] + contig_id + '.fasta.gz', 'r') as f:
+      fa = [l.strip().decode('utf-8') for l in f]
+      fa.pop(0)
+    with open(strain + '.fasta', 'a') as g:
+      g.write('\n'.join(fa))
+
+
+if __name__ == '__main__':
+  main()
+      
+    
